ca5347f26d073b71d2ca29604915060370367260
black-tul1p <divay7@gmail.com>
2023-01-21 03:59:57 -0500

    Fixed CSS

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ export default function Home() {
   // Hard coded for now
   // const [shows, setShows] = useState([]);
   const [shows, setShows] = useState([
+    "Fleabag",
     "Queen's Gambit",
     "The Last of Us",
     "Avatar: The Way of Water",
@@ -12,8 +13,23 @@ export default function Home() {
     "The Office",
   ]);
 
+  // Hard coded for now
+  // const [pop, setPop] = useState([]);
+  const [pop, setPop] = useState([
+    "One Piece",
+    "Wednesday",
+    "Bojack Horseman",
+    "The Office",
+    "The Last of Us",
+  ]);
+
   // TODO: Finish API and complete this
   const fetchWatched = () => {
+
+  };
+
+  // TODO: Finish API and complete this
+  const fetchPopular = () => {
 
   };
 
@@ -21,13 +37,23 @@ export default function Home() {
     <>
       <div className="Home-content">
         <div className="Home-box">
-          <h1>Search Placeholder</h1>
-          <SearchBar />
+          <div>
+            <h1>Collect More</h1>
+            <SearchBar />
+          </div>
+          <div>
+            <h1> Popular Right Now</h1>
+            <div className="Pop-list">
+            {pop?.map(element => {
+              return <p>{element}</p>;
+            })}
+            </div>
+          </div>
         </div>
-        <div className="Home-box">
+        <div className="Home-box" style={{ gap: 0 }}>
           <h1>Watch List</h1>
           <ol className="Watch-list">
-            {shows?.map((element, idx) => {
+            {shows?.map(element => {
               return <li>{element}</li>;
             })}
           </ol>
