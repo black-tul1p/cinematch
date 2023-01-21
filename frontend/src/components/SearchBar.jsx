55c80fa5b002de5d75f751ebaf7eae4c5e9139d9
black-tul1p <divay7@gmail.com>
2023-01-21 03:59:57 -0500

    Fixed CSS

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 
 export default function SearchBar() {
+    const [query, setQuery] = useState("");
+
+    // TODO: Update with actual API call
+    const submitQuery = () => {
+        alert("Submitted!");
+    };
+
     return (
-        <div>
-            BAR
+        <div className="Search-bar">
+            <form>
+                {console.log(query)}
+                <input className="Search-input" type="text" placeholder="Search..." onSubmit={submitQuery} onChange={(event) => {setQuery(event.target.value);}}/>
+                <button type="submit" onClick={submitQuery}>Add</button>
+            </form>
         </div>
     );
 ;}
