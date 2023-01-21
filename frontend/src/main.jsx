a9d3d24651cf85e65f7799de29eefea247a080d7
Dakshesh <daksheshgupta03@gmail.com>
2023-01-20 22:30:46 -0500

    basic routing, navigation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+import Navigation from './components/Navigation';
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter >
+    <Navigation />
+      <Routes>
+        <Route path='/' element={<App />}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
