fe341f2ad1710eb21f5965bfe60ea9aef34801ac
black-tul1p <divay7@gmail.com>
2023-01-22 00:56:30 -0500

    Improved Watch List CSS

--- frontend/src/components/WatchList.jsx
+++ frontend/src/components/WatchList.jsx
@@ -1,51 +1,33 @@
 import { useContext } from "react";
 import { collection, QuerySnapshot } from "firebase/firestore";
-import { useCollection } from 'react-firebase-hooks/firestore';
+import { useCollection } from "react-firebase-hooks/firestore";
 
 import { AuthContext } from "./AuthContext";
-import { FireStoreDB } from '../main';
+import { FireStoreDB } from "../main";
 
 export default function WatchList() {
-
   const { user } = useContext(AuthContext);
 
-  const [value, loading, error] = useCollection(collection(FireStoreDB, `users/${user.uid}/movies`))
-
-  const imageStyle = {
-    height: "100px",
-    width: "100px",
-  };
-
-  const boxStyle = {
-    height: "150px",
-    width: "300px",
-  }
-
+  const [value, loading, error] = useCollection(
+    collection(FireStoreDB, `users/${user.uid}/movies`)
+  );
 
   return (
-    <ol className="Watch-list">
+    <div className="Watch-list">
       {loading && <span>Loading...</span>}
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
-      {value && value.docs.map((doc) => {
-        return <li key={doc.id} style = {boxStyle}>
-                <ul class="sub-menu" type="none">
-                  <li>
-                    {doc.data().title}
-                  </li>
-                  <li>
-                    <img src={doc.data().poster} style = {imageStyle} />
-                  </li>
-                  <li>
-                    {doc.data().year}
-                  </li>
-                </ul>
-               </li>
-      })}
-    </ol>
-  )
-
-
+      {value &&
+        value.docs.map((doc) => {
+          return (
+            <div key={doc.id} className="Movie-item">
+              <div className="Movie-info">
+                <div id="title-name">{doc.data().title}</div>
+                <div id="title-year">{doc.data().year}</div>
+              </div>
+              <img src={doc.data().poster} id="title-image" />
+            </div>
+          );
+        })}
+    </div>
+  );
 }
-
-
-
